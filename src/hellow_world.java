7b6d6a661023818f7df2f052bb11b198870487e4
Jiahang Zhang <jiahang@usc.edu>
2021-07-13 17:00:39 +0800

    new update

--- src/hellow_world.java
+++ src/hellow_world.java
@@ -2,5 +2,6 @@ public class hellow_world {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Are you OK?");
+        System.out.println("I'm ok");
     }
 }
